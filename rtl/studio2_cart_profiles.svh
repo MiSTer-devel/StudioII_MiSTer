a75bfb69dc9d21a2c0c4418c46cf7e7459dee6bd
meauxdal <68095521+meauxdal@users.noreply.github.com>
2026-09-19 00:59:40 -0400

    profiles update pt 2
    race v4

--- rtl/studio2_cart_profiles.svh
+++ rtl/studio2_cart_profiles.svh
@@ -311,8 +311,8 @@
 
 	end
 
-// Race / Race Colour v1/v2/v3
-16'h1CA7, 16'h47EA, 16'h5374, 16'h5638, 16'h6664,
+// Race / Race Colour v1/v2/v3/v4
+16'h17DC, 16'h1CA7, 16'h47EA, 16'h5374, 16'h5638, 16'h6664,
 16'h797C, 16'hC713, 16'hD6C0, 16'hFCC8:
 	begin
 
